ebe9016f83324ccf0d3a3e51b1fae849b2584634
pashunt13 <pavelkozencev2002@gmail.com>
2023-11-14 11:40:27 +0300

    Updates

--- src/pages/HomeWork1/HomeWork1.tsx
+++
@@ -1,3 +0,0 @@
-export const HomeWork1 = () => {
-  return <div>Решение домашнего задания №1</div>;
-};
--- src/pages/HomeWork2/HomeWork2.tsx
+++
@@ -1,3 +0,0 @@
-export const HomeWork2 = () => {
-  return <div>Решение домашнего задания №2</div>;
-};
