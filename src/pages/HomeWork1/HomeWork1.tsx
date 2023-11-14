b6179306719e7f0fa5aa4c645f4738ad69343f7b
pashunt13 <pavelkozencev2002@gmail.com>
2023-11-14 16:24:29 +0300

    dice roll by images

--- src/pages/HomeWork1/HomeWork1.tsx
+++ src/pages/HomeWork1/HomeWork1.tsx
@@ -1,3 +1,33 @@
+import { useState } from "react";
+import './style.css'
+
+interface DiseProps {
+  faceNum: number;
+}
+
 export const HomeWork1 = () => {
-  return <div>Решение домашнего задания №1</div>;
+  const [faceNum, setFaceNum] = useState(6);
+
+  const handleClick = () => {
+    setFaceNum(getRandom(1, 6));
+  }
+
+  return (
+    <div className="container">
+      <Dice faceNum={faceNum} />
+      <button className="roll" onClick={handleClick}>Roll the dice</button>
+    </div>
+  );
 };
+
+const Dice = ({ faceNum }: DiseProps) => {
+  return ( 
+    <img 
+      src={`/react-hometasks/assets/images/dice-six-faces-${faceNum}.png`}
+      alt="dice" 
+      className="dice" 
+    />
+  )
+}
+
+const getRandom = (min: number, max: number) => Math.floor(min + Math.random() * (max + 1 - min));
