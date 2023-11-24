7551eee8013eef272e3cea7930814c9a515d8f1b
pashunt13 <pavelkozencev2002@gmail.com>
2023-11-24 13:01:13 +0300

    scrolling first ver

--- src/pages/HomeWork1/HomeWork1.tsx
+++ src/pages/HomeWork1/HomeWork1.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import './style.css'
+import { useState } from 'react';
+import styles from './HomeWork1.module.css';
 
 interface DiseProps {
   faceNum: number;
@@ -10,24 +10,27 @@ export const HomeWork1 = () => {
 
   const handleClick = () => {
     setFaceNum(getRandom(1, 6));
-  }
+  };
 
   return (
-    <div className="container">
+    <div className={styles.container}>
       <Dice faceNum={faceNum} />
-      <button className="roll" onClick={handleClick}>Roll the dice</button>
+      <button className={styles.roll} onClick={handleClick}>
+        Roll the dice
+      </button>
     </div>
   );
 };
 
 const Dice = ({ faceNum }: DiseProps) => {
-  return ( 
-    <img 
+  return (
+    <img
       src={`/react-hometasks/assets/images/dice-six-faces-${faceNum}.png`}
-      alt="dice" 
-      className="dice" 
+      alt='dice'
+      className={styles.dice}
     />
-  )
-}
+  );
+};
 
-const getRandom = (min: number, max: number) => Math.floor(min + Math.random() * (max + 1 - min));
+const getRandom = (min: number, max: number) =>
+  Math.floor(min + Math.random() * (max + 1 - min));
